90891e601c3a411ca0d9179a1b751cb8d44d7263
a.beethovn <a.beethovn@gmail.com>
2017-02-05 15:08:40 -0800

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'omb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'omb works!';
+}
